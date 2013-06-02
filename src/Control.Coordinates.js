4028bb1566b9c551b0acf2e9af6db3587a7ac841
Felix Bache <felix.bache@gmail.com>
2013-06-02 22:49:33 +0200

    added css styles and configurable jump to coordinate funcitonality

--- src/Control.Coordinates.js
+++ src/Control.Coordinates.js
@@ -7,20 +7,128 @@ L.Control.Coordinates = L.Control.extend({
 		position: 'bottomright',
 		decimals:4,
 		decimalSeperator:".",
-		labelTemplate:"Lat: {x} Lng:{y}"
+		labelTemplateLat:"Lat: {y}",
+		labelTemplateLng:"Lng: {x}",
+		enableUserInput:true
 	},
 
 	onAdd: function (map) {
 		this._map = map;
 
 		var className = 'leaflet-control-coordinates',
-		    container = this._container= L.DomUtil.create('div', className),
-		    options = this.options;
+			container = this._container = L.DomUtil.create('div', className),
+			options = this.options;
+
+		//label containers
+		this._labelcontainer = L.DomUtil.create("div", "uiElement label", container);
+		this._labelX=L.DomUtil.create("span", "labelX", this._labelcontainer);
+		this._labelY=L.DomUtil.create("span", "labelY", this._labelcontainer);
+
+
+		//input containers
+		this._inputcontainer = L.DomUtil.create("div", "uiElement input uiHidden", container);
+		this._inputX=this._createInput("inputX", this._inputcontainer);
+		this._inputY=this._createInput("inputY", this._inputcontainer);
+		L.DomEvent.on(this._inputX, 'keyup', this._handleKeypress, this);
+		L.DomEvent.on(this._inputY, 'keyup', this._handleKeypress, this);
 
 		map.on("mousemove", this._update, this);
+		map.on('dragstart', this.collapse, this);
+
+		this._marker = L.marker();
+		this._marker.on("click",this._clearMarker,this);
+
 		map.whenReady(this._update,this);
 
+		this._showsCoordinates=true;
+		if (options.enableUserInput) {
+			L.DomEvent.addListener(this._container, "click",this._switchUI,this);
+		}
+
 		return container;
+	},
+
+	_createInput : function(classname, container) {
+		var input = L.DomUtil.create("input", classname, container);
+		input.type="text";
+		L.DomEvent.disableClickPropagation(input);
+		return input;
+	},
+
+	_clearMarker : function() {
+		this._map.removeLayer(this._marker);
+		this.collapse();
+	},	
+
+	_handleKeypress : function(e) {
+		switch(e.keyCode)
+		{
+			case 27: //Esc
+				this.collapse();
+			break;
+			default://All keys
+				this._handleSubmit();
+			break;
+		}
+	},
+
+	_handleSubmit : function()  {
+		var x = this._createValidInput(this._inputX.value);
+		var y = this._createValidInput(this._inputY.value);
+		if (x!=undefined&&y!=undefined){
+			this._marker.setLatLng(new L.LatLng(y, x));
+			this._marker.addTo(this._map);
+		}
+	},
+
+	_createValidInput:function(num){
+		if (num&&num.length>0){
+			var numbers = num.split(this.options.decimalSeperator);
+			try{
+
+				var numRes=Number(numbers.join("."));
+				if(isNaN(numRes)){
+					return undefined;
+				}
+				return numRes;
+			}catch(e){
+				return undefined;
+			}
+		}
+		return undefined;
+	},
+
+	expand:function() {
+		this._showsCoordinates=false;
+
+		L.DomEvent.addListener(this._container,"mousemove",L.DomEvent.stop);
+		L.DomEvent.removeListener(this._container, "click",this._switchUI,this);
+
+		L.DomUtil.addClass(this._labelcontainer, "uiHidden");
+		L.DomUtil.removeClass(this._inputcontainer, "uiHidden");
+	},
+
+	collapse:function() {
+		this._showsCoordinates=true;
+
+		L.DomEvent.addListener(this._container, "click",this._switchUI,this);
+		L.DomEvent.removeListener(this._container,"mousemove",L.DomEvent.stop);
+
+		L.DomUtil.addClass(this._inputcontainer, "uiHidden");
+		L.DomUtil.removeClass(this._labelcontainer, "uiHidden");
+	},
+
+	_switchUI : function(evt){
+		L.DomEvent.stop(evt);
+		L.DomEvent.stopPropagation(evt);
+		L.DomEvent.preventDefault(evt);
+		if (this._showsCoordinates) {
+			//show textfields
+			this.expand();
+		}else {
+			//show coordinates
+			this.collapse();
+		}
 	},
 
 	onRemove: function (map) {
@@ -29,20 +137,26 @@ L.Control.Coordinates = L.Control.extend({
 
 	_update: function (evt) {
 		var pos=evt.latlng,
-		label="",
-		opts = this.options;
+		labelX="",
+		labelY="",
+		opts = this.options,
+		x="",
+		y="";
 		if (pos) {
-			label = L.Util.template(opts.labelTemplate, {
-				x:this._getRoundNum(pos.lng),
-				y:this._getRoundNum(pos.lat),
-			});
-		} else {
-			label = L.Util.template(opts.labelTemplate, {
-				x:"",
-				y:"",
-			});
+			this._currentPos=pos;
+			y=this._getRoundNum(pos.lat);
+			x=this._getRoundNum(pos.lng);
+			this._inputX.value=x;
+			this._inputY.value=y;
 		}
-		this._container.innerHTML = label;
+		labelY = L.Util.template(opts.labelTemplateLat, {
+			y:y
+		});
+		labelX = L.Util.template(opts.labelTemplateLng, {
+			x:x
+		});
+		this._labelX.innerHTML = labelX;
+		this._labelY.innerHTML = labelY;
 	},
 
 	_getRoundNum: function (num) {
@@ -54,7 +168,7 @@ L.Control.Coordinates = L.Control.extend({
 			var d = decimals-numbers[1].length;
 			for (; d > 0; d--) {
 				numbers[1]+="0";
-			};
+			}
 			res = numbers.join(opts.decimalSeperator);
 		}
 		return res;
